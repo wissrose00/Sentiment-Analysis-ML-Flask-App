5eb7e03c0c51d0eabf352c1204ca96ef64bc6083
Dico <75106345+wissrose00@users.noreply.github.com>
2023-11-24 21:51:27 +0700

    Update app.py

--- Sentiment-Analysis-ML-Flask-App/app.py
+++ Sentiment-Analysis-ML-Flask-App/app.py
@@ -18,7 +18,6 @@ def my_form():
     return render_template('form.html')
 
 @app.route('/', methods=['POST'])
-
 def my_form_post():
     stop_words = stopwords.words('english')
     
@@ -27,9 +26,6 @@ def my_form_post():
     
     text_final = ''.join(c for c in text1 if not c.isdigit())
     
-    #remove punctuations
-    #text3 = ''.join(c for c in text2 if c not in punctuation)
-        
     #remove stopwords    
     processed_doc1 = ' '.join([word for word in text_final.split() if word not in stop_words])
 
@@ -40,4 +36,4 @@ def my_form_post():
     return render_template('form.html', final=compound, text1=text_final,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
 
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=5555)
+    app.run(debug=True, host="0.0.0.0", port=55555)
